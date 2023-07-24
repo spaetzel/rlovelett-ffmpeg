c76b3995b279ca83a667512b932765dbe1b3a8e7
Justin Kyryliuk <justin.kyryliuk@vidyard.com>
2023-07-24 10:24:25 -0400

    [CRAP-329] Increase Analyze Duration From Default 5s To 10s

--- lib/ffmpeg/black_detect.rb
+++ lib/ffmpeg/black_detect.rb
@@ -15,7 +15,7 @@ module FFMPEG
 
     def run
       # ffmpeg will output to stderr
-      command = "#{FFMPEG.ffprobe_binary} -hide_banner -f lavfi -i \"movie=#{Shellwords.escape(@movie.path)},blackdetect[out0]\" -show_entries tags=lavfi.black_start,lavfi.black_end -of default=nw=1 -v quiet"
+      command = "#{FFMPEG.ffprobe_binary} -hide_banner -analyzeduration 10000000 -probesize 10000000 -f lavfi -i \"movie=#{Shellwords.escape(@movie.path)},blackdetect[out0]\" -show_entries tags=lavfi.black_start,lavfi.black_end -of default=nw=1 -v quiet"
       std_output = ''
       std_error = ''
 
