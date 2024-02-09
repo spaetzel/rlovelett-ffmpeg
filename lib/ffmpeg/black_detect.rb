73024b1d9a20f427e20b7d72bbee9ed6d1b6bb98
Will Spaetzel <will.spaetzel@vidyard.com>
2024-02-09 11:17:06 -0500

    Force a pr change

--- lib/ffmpeg/black_detect.rb
+++ lib/ffmpeg/black_detect.rb
@@ -19,11 +19,6 @@ module FFMPEG
       std_output = ''
       std_error = ''
 
-      Open3.popen3(command) do |stdin, stdout, stderr|
-        std_output = stdout.read unless stdout.nil?
-        std_error = stderr.read unless stderr.nil?
-      end
-
       fix_encoding(std_output)
       fix_encoding(std_error)
 
